359e5e585ddf4916f2215da9b931a0cc3e2aac69
AlejoPal <alejandro.palacios.apf2000@gmail.com>
2023-05-27 23:39:09 -0500

    se corrige el error del gradle y funciona de nuevo la app

--- app/src/main/java/com/aproyectousco/valornutricionalalimentoscolombianos/AgregarInfo.java
+++ app/src/main/java/com/aproyectousco/valornutricionalalimentoscolombianos/AgregarInfo.java
@@ -5,18 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.model.ValueRange;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.GeneralSecurityException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class AgregarInfo extends AppCompatActivity {
