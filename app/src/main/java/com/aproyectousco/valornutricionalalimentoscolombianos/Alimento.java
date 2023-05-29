c682aae5e73d749c9bdbc24a3b079362e6fc2b35
AlejoPal <alejandro.palacios.apf2000@gmail.com>
2023-05-29 15:38:59 -0500

    se vuelve a poner el contructor porque si era necesario

---
+++ app/src/main/java/com/aproyectousco/valornutricionalalimentoscolombianos/Alimento.java
@@ -0,0 +1,80 @@
+package com.aproyectousco.valornutricionalalimentoscolombianos;
+
+public class Alimento {
+    private String nombre;
+    private String energia;
+    private String proteina;
+    private String carbohidratos;
+    private String colesterol;
+    private String lipidos;
+    private String gsat;
+    private String sodio;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEnergia() {
+        return energia;
+    }
+
+    public void setEnergia(String energia) {
+        this.energia = energia;
+    }
+
+    public String getProteina() {
+        return proteina;
+    }
+
+    public void setProteina(String proteina) {
+        this.proteina = proteina;
+    }
+
+    public String getCarbohidratos() {
+        return carbohidratos;
+    }
+
+    public void setCarbohidratos(String carbohidratos) {
+        this.carbohidratos = carbohidratos;
+    }
+
+    public String getColesterol() {
+        return colesterol;
+    }
+
+    public void setColesterol(String colesterol) {
+        this.colesterol = colesterol;
+    }
+
+    public String getLipidos() {
+        return lipidos;
+    }
+
+    public void setLipidos(String lipidos) {
+        this.lipidos = lipidos;
+    }
+
+    public String getGsat() {
+        return gsat;
+    }
+
+    public void setGsat(String gsat) {
+        this.gsat = gsat;
+    }
+
+    public String getSodio() {
+        return sodio;
+    }
+
+    public void setSodio(String sodio) {
+        this.sodio = sodio;
+    }
+
+    // Constructor y getters/setters
+    // ...
+}
+
