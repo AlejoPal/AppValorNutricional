6e0b1f5b09a9a4cfcc0ff199fcf93fa86e2e486a
AlejoPal <alejandro.palacios.apf2000@gmail.com>
2023-05-28 20:19:51 -0500

    Se puede agregar la busqueda a la base de datos con una api de google sheet. Falta obtener los valores correspondientes

--- app/src/main/java/com/aproyectousco/valornutricionalalimentoscolombianos/AgregarInfo.java
+++ app/src/main/java/com/aproyectousco/valornutricionalalimentoscolombianos/AgregarInfo.java
@@ -2,11 +2,22 @@ package com.aproyectousco.valornutricionalalimentoscolombianos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import java.util.ArrayList;
 import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class AgregarInfo extends AppCompatActivity {
 
@@ -32,6 +43,71 @@ public class AgregarInfo extends AppCompatActivity {
 
     }
 
+
+
+
+    @SuppressLint("StaticFieldLeak")
+    private void obtenerDatosColumnaDesdeSheets() {
+        new AsyncTask<Void, Void, List<String>>() {
+            @Override
+            protected List<String> doInBackground(Void... voids) {
+                // URL de la API con el spreadsheetId y sheet
+                String apiUrl = "https://script.google.com/macros/s/AKfycbyDpgPAZrRnAlK-HnO0mRbjxo8BscDDX1td3Z9tyUpkl-9ZMdlwGd7PmCLrtMBt7o2k/exec?spreadsheetId=1bGG7LkQRdh73Y81ZlZ8_Yusb4xuutc3cf_HeJEmkoFc&sheet=nutricion";
+
+                try {
+                    // Realizar solicitud HTTP GET a la API
+                    URL url = new URL(apiUrl);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.connect();
+
+                    // Leer la respuesta de la API
+                    InputStream inputStream = connection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        response.append(line);
+                    }
+
+                    // Analizar la respuesta JSON
+                    JSONObject jsonResponse = new JSONObject(response.toString());
+                    JSONArray nutricionArray = jsonResponse.getJSONArray("nutricion");
+
+                    // Recorrer el arreglo de objetos nutricion y extraer los valores de cada objeto
+                    List<String> valuesList = new ArrayList<>();
+                    for (int i = 0; i < nutricionArray.length(); i++) {
+                        JSONObject nutricionObj = nutricionArray.getJSONObject(i);
+                        String nombre = nutricionObj.getString("Nombre");
+                        valuesList.add(nombre);
+                    }
+
+                    return valuesList;
+
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(List<String> result) {
+                if (result != null) {
+                    valuesList = result;
+
+                    // Configurar el ArrayAdapter con la lista de valores
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(AgregarInfo.this, android.R.layout.simple_dropdown_item_1line, valuesList);
+
+                    // Asignar el adaptador al AutoCompleteTextView
+                    autoCompleteTextView.setAdapter(adapter);
+                }
+            }
+        }.execute();
+    }
+
+
+    /*
     private void obtenerDatosColumnaDesdeSheets() {
         // Aquí puedes implementar tu lógica para obtener los valores de la columna desde Google Sheets
         // Puedes usar una biblioteca como Sheets API de Google o HTTP Request para realizar la consulta a Google Sheets y obtener los valores
@@ -47,7 +123,7 @@ public class AgregarInfo extends AppCompatActivity {
         // Puedes almacenar los valores en la lista valuesList
     }
 
-    /*
+
     private void obtenerDatosColumnaDesdeSheets() throws GeneralSecurityException, IOException {
 
 
