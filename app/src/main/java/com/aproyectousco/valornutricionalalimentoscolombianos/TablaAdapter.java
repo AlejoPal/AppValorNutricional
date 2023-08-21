75d923a6fc170ce3a21d91d2d10c229e16fff47e
AlejoPal <alejandro.palacios.apf2000@gmail.com>
2023-08-21 00:53:27 -0500

    se elimina cada elemento de firebase al presionarl la x pero no se actualiza en el viewpager, toca cuadrar esa funcionalidad

--- app/src/main/java/com/aproyectousco/valornutricionalalimentoscolombianos/TablaAdapter.java
+++ app/src/main/java/com/aproyectousco/valornutricionalalimentoscolombianos/TablaAdapter.java
@@ -1,5 +1,6 @@
 package com.aproyectousco.valornutricionalalimentoscolombianos;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,8 +9,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -32,6 +36,8 @@ public class TablaAdapter extends RecyclerView.Adapter<TablaAdapter.TablaViewHol
     public void onBindViewHolder(@NonNull TablaViewHolder holder, int position) {
         TablaItem item = tablaItemList.get(position);
 
+        final boolean[] isButtonEnabled = {true};
+
         // Asignar los valores de TablaItem a las vistas en la fila del RecyclerView
         holder.txtAlimentos.setText(item.getComidas());
         holder.txtcarbohidratos.setText(item.getCarbohidratos());
@@ -43,22 +49,115 @@ public class TablaAdapter extends RecyclerView.Adapter<TablaAdapter.TablaViewHol
         holder.txtsodio.setText(item.getSodio());
 
         // Agregar el OnClickListener al botón de eliminación
+
         holder.btnImagen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Eliminar el elemento de la lista y notificar cambios
-                tablaItemList.remove(position);
-                notifyItemRemoved(position);
-                notifyItemRangeChanged(position, tablaItemList.size());
 
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference mRootReference = database.getReference();
 
-                // Aquí debes implementar la lógica para eliminar el elemento de la base de datos (Firebase)
-                DatabaseReference databaseRef = mRootReference.child("Usuario").child(item.getCorreo()).child(item.getFecha()).child(item.getTiempo()).child("Alimentos").child(item.getComidas());
-                databaseRef.removeValue();
+
+                if (isButtonEnabled[0]) {
+                    isButtonEnabled[0] = false;
+
+                    // Deshabilita el botón durante 1 segundo
+                    holder.btnImagen.setEnabled(false);
+
+                    // Crea un temporizador para habilitar el botón después de 1 segundo
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            holder.btnImagen.setEnabled(true);
+                            isButtonEnabled[0] = true;
+                        }
+                    }, 2000); // 1000 milisegundos = 1 segundo
+
+
+
+                    int currentPosition = holder.getAdapterPosition(); // Obtén la posición actual del elemento
+                    if (currentPosition != RecyclerView.NO_POSITION) { // Verifica si la posición es válida
+                        TablaItem itemToRemove = tablaItemList.get(currentPosition);
+
+                        // Resto del código para eliminar el elemento y actualizar los valores
+                        final double[] totalCarbohidratos = {0};
+                        final double[] totalColesterol = {0};
+                        final double[] totalEnergia = {0};
+                        final double[] totalGsat = {0};
+                        final double[] totalLipidos = {0};
+                        final double[] totalProteinas = {0};
+                        final double[] totalSodio = {0};
+
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        DatabaseReference mRootReference = database.getReference();
+
+                        // Obtén el elemento a eliminar
+
+
+                        // Obtén la referencia al nodo "Alimentos" en la base de datos
+                        DatabaseReference alimentosRef = mRootReference.child("Usuario")
+                                .child(itemToRemove.getCorreo())
+                                .child(itemToRemove.getFecha())
+                                .child(itemToRemove.getTiempo())
+                                .child("Alimentos");
+
+
+                        // Agrega un listener para obtener los valores de cada alimento y sumarlos
+                        alimentosRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                                for (DataSnapshot alimentoSnapshot : dataSnapshot.getChildren()) {
+                                    // Obtén los valores de cada alimento y suma a los totales
+                                    double carbohidratos = alimentoSnapshot.child("Carbohidratos").getValue(Double.class);
+                                    double colesterol = alimentoSnapshot.child("Colesterol").getValue(Double.class);
+                                    double energia = alimentoSnapshot.child("Energia").getValue(Double.class);
+                                    double gsat = alimentoSnapshot.child("Gsat").getValue(Double.class);
+                                    double lipidos = alimentoSnapshot.child("Lipidos").getValue(Double.class);
+                                    double proteinas = alimentoSnapshot.child("Proteina").getValue(Double.class);
+                                    double sodio = alimentoSnapshot.child("Sodio").getValue(Double.class);
+
+                                    totalCarbohidratos[0] += carbohidratos;
+                                    totalColesterol[0] += colesterol;
+                                    totalEnergia[0] += energia;
+                                    totalGsat[0] += gsat;
+                                    totalLipidos[0] += lipidos;
+                                    totalProteinas[0] += proteinas;
+                                    totalSodio[0] += sodio;
+                                }
+
+                                // Actualiza los valores en el nodo "Resumen" en Firebase
+                                DatabaseReference resumenRef = mRootReference.child("Usuario")
+                                        .child(itemToRemove.getCorreo())
+                                        .child(itemToRemove.getFecha())
+                                        .child(itemToRemove.getTiempo());
+
+
+                                resumenRef.child("Carbohidratos").setValue(totalCarbohidratos[0]);
+                                resumenRef.child("Colesterol").setValue(totalColesterol[0]);
+                                resumenRef.child("Energia").setValue(totalEnergia[0]);
+                                resumenRef.child("Gsat").setValue(totalGsat[0]);
+                                resumenRef.child("Lipidos").setValue(totalLipidos[0]);
+                                resumenRef.child("Proteina").setValue(totalProteinas[0]);
+                                resumenRef.child("Sodio").setValue(totalSodio[0]);
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                                // Manejo de errores en caso de fallo en la lectura de la base de datos
+                            }
+                        });
+
+                        tablaItemList.remove(currentPosition);
+                        notifyItemRemoved(currentPosition);
+                        // Aquí debes implementar la lógica para eliminar el elemento de la base de datos (Firebase)
+                        DatabaseReference databaseRef = mRootReference.child("Usuario").child(item.getCorreo()).child(item.getFecha()).child(item.getTiempo()).child("Alimentos").child(item.getComidas());
+                        databaseRef.removeValue();
+                    }
+
+                }
             }
         });
+
     }
 
     @Override
